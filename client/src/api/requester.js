e4734d237fb839c0dc14af40018958f1f51008f6
Galina <baleva1990@gmail.com>
2024-07-22 00:07:15 +0300

    Add abstract requester

---
+++ client/src/api/requester.js
@@ -0,0 +1,27 @@
+async function requester(method, url, data) {
+    const options = {};
+
+    if(data){
+        options.headers = {
+            'Content-Type': 'application/json',
+        },
+        options.body = JSON.stringify(data); 
+    }
+    try{
+        const respons = await fetch(url, options);
+        const gamesData = await respons.json();
+
+        return gamesData;
+
+
+    } catch(err){
+        return alert(err.message);
+    }
+
+
+}
+
+export const get = requester.bind(null, 'get');
+export const post = requester.bind(null, 'post');
+export const put = requester.bind(null, 'put');
+export const del = requester.bind(null, 'delete');
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -5,21 +5,21 @@ export default function Header() {
         <header>
             {/* Navigation */}
             <h1>
-                <Link className="home" href="/">
+                <Link className="home" to="/">
                     GamesPlay
                 </Link>
             </h1>
             <nav>
-                <Link href="/games">All games</Link>
+                <Link to="/games">All games</Link>
                 {/* Logged-in users */}
                 <div id="user">
-                    <Link href="/games/create">Create Game</Link>
-                    <Link href="#">Logout</Link>
+                    <Link to="/games/create">Create Game</Link>
+                    <Link to="#">Logout</Link>
                 </div>
                 {/* Guest users */}
                 <div id="guest">
-                    <Link href="/login">Login</Link>
-                    <Link href="/register">Register</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
                 </div>
             </nav>
         </header>
